e26241e402eb3767f1faee82c494462970e5ef7f
Tafina <66565611+Tafina@users.noreply.github.com>
2020-08-19 23:51:51 +0300

    Add files via upload

---
+++ functions/listeners.js
@@ -0,0 +1,7 @@
+const fetch = require("node-fetch");
+
+module.exports = async function(){
+    const res = await fetch("https://api.dualfm.net/stats").then(res => res.json());
+    return `${res.listeners.current}`
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.artist = require('./functions/artist');
+module.exports.listeners = require('./functions/listeners');
+module.exports.listeners_peak = require('./functions/listeners_peak');
+module.exports.nowplaying = require('./functions/nowplaying');
+module.exports.presenter = require('./functions/presenter');
+module.exports.version = require('./functions/version');
