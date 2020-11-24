2a8d105c606aeddede5219cd5e8a0e45297f31e1
Marino <48334228+marinofranz@users.noreply.github.com>
2020-11-24 13:59:42 +0000

    add new functions

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ module.exports.listeners = require('./functions/listeners');
 module.exports.listeners_peak = require('./functions/listeners_peak');
 module.exports.nowplaying = require('./functions/nowplaying');
 module.exports.presenter = require('./functions/presenter');
+module.exports.message = require('./functions/dj_message');
+module.exports.colour = require('./functions/dj_colour');
 module.exports.version = require('./functions/version');
